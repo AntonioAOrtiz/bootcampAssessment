d5b57c30727ebfb922e9caf59c1447026ac9f217
Antonio Ortiz <antonio.ortiz@solera.com>
2022-11-17 10:40:10 +0100

    create custom model

---
+++ src/main/java/com/bootcampAssessment/codingExercise/Model/Custom.java
@@ -0,0 +1,85 @@
+package com.bootcampAssessment.codingExercise.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Custom {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String phoneNumber;
+	
+	@Column
+	private String email;
+	
+	public Custom(long id, String firstName, String lastName, String phoneNumber, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Custom [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
+				+ phoneNumber + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+}

2dc342e33b3aaf5a4dc021ce73e74a1577df69e7
Antonio Ortiz <antonio.ortiz@solera.com>
2022-11-17 15:32:55 +0100

    custom CRUD

--- src/main/java/com/bootcampAssessment/codingExercise/Controller/AppController.java
+++ src/main/java/com/bootcampAssessment/codingExercise/Controller/AppController.java
@@ -1,11 +1,15 @@
 package com.bootcampAssessment.codingExercise.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,15 +21,37 @@ import com.bootcampAssessment.codingExercise.Service.CustomService;
 public class AppController {
 	
 	@Autowired
-	CustomService customservice;
+	CustomService customService;
 	
 	@GetMapping(path = "/customs")
 	public List<Custom> getCustoms() {
-		return customservice.getAllCustoms();
+		return customService.getAllCustoms();
+	}
+	
+	@GetMapping(path = "/customs/{id}")
+	public Optional<Custom> getCustomsById(@PathVariable("id") long id) {
+		return customService.getCustomById(id);
 	}
 	
 	@PostMapping(path = "/custom")
 	public void sendCustoms(@RequestBody Custom custom) {
-		customservice.addCustom(custom);
+		customService.addCustom(custom);
 	}
+	
+	@PutMapping("/custom/{id}")
+	public Custom updateCustom(@PathVariable long id, @RequestBody Custom custom) {
+		return customService.updateCustom(custom, id);
+	}
+	
+	@DeleteMapping("/custom/{id}")
+	public void deleteCustom(@PathVariable long id) {
+		customService.deleteCustom(id);
+	}
+	
+	@DeleteMapping("/customs")
+	public void deleteAllCustoms() {
+		customService.deleteAllCustoms();
+	}
+	
+	
 }
--- src/main/java/com/bootcampAssessment/codingExercise/Service/CustomService.java
+++ src/main/java/com/bootcampAssessment/codingExercise/Service/CustomService.java
@@ -1,6 +1,7 @@
 package com.bootcampAssessment.codingExercise.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,38 @@ public class CustomService {
 	
 	public List<Custom> getAllCustoms() {
 		return repo.findAll();
+	}
+	
+	public Optional<Custom> getCustomById(long id) {
+		return repo.findById(id);
+	}
+	
+	public Custom updateCustom(Custom updatedCustom,long id) {
+		Optional<Custom> custom = repo.findById(id);
+		
+		if (custom.isPresent()) {
+			Custom finalCustom = custom.get();
+			
+			finalCustom.setFirstName(updatedCustom.getFirstName());
+			finalCustom.setLastName(updatedCustom.getLastName());
+			finalCustom.setPhoneNumber(updatedCustom.getPhoneNumber());
+			finalCustom.setEmail(updatedCustom.getEmail());
+			
+			return repo.save(updatedCustom);
+		}
+			return null;
+	}
+	
+	public void deleteCustom(long id) {
+		Optional<Custom> custom = repo.findById(id);
+		
+		if (custom.isPresent()) {
+			repo.deleteById(id);
+		}
+		
+	}
+	
+	public void deleteAllCustoms( ) {
+		repo.deleteAll();
 	}
 }
